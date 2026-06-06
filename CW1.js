ec5febca295145be929521e728ef0660b418fb32
Saroj lama <tamangsarooz100@gmail.com>
2026-06-06 18:13:36 +0545

    Data Type part 1

---
+++ CW1.js
@@ -0,0 +1,34 @@
+// 1. Sum of Two Numbers
+// Create two variables and store numbers in them. Print their sum.
+let num1 = 2;
+let num2 = 2;
+
+console.log(`The sum of ${num1 + num2}`);
+
+// 2. Calculate Age Next Year
+// Store your current age in a variable and print what your age will be next year.
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter your birth year: ", (birthYear) => {
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - Number(birthYear);
+
+    console.log(`Your age is ${age}`);
+
+    rl.close();
+});
+// 3. Calculate Total Price
+// Store the price of a book and the quantity purchased. Calculate and print the total cost.
+
+
+// 4. Find the Average
+// Store three test scores in variables and calculate their average.
+
+
+// 5. Convert Minutes to Seconds
+// Store a number of minutes in a variable and convert it to seconds.
--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 ## Javascript
 
+Day 1
 # Introduction
 - Javascript is the web base programming language
 
 # Runtime
 - We need node js for running this
+
+
+Day 2
+# Data type:
+-There are 2 types of data
+1. Primitive Datatype
+2. Non-Primitive Datatype
+
+1. Primitive Datatype:
+- String - We can just use Alphabat 
+- Number - We can just use Number here
+- Boolean 
+- Undifine 
+- Null 
+- Bigint
+- Symbol
+
+2. Non- Primitive Datatype
+
+- Array
+- Object
 
 
