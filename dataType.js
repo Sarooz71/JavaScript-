a97ecb5069e99cd5603a73ad87c941d337c6cdd9
Saroj lama <tamangsarooz100@gmail.com>
2026-06-10 20:56:10 +0545

    Data type done

--- dataType.js
+++ dataType.js
@@ -2,15 +2,25 @@
 const name = "Saroj Lama";
 let location = "Kirtipur";
 let perAddress = "Nuwakot"
-
 console.log(`My name is ${name}.I live in ${location}.I came from ${perAddress}`);
-
 
 // Number 
 let age = 61;
 let price = 100;
-
 console.log(`my name is Rahul and i am ${age} years old` );
 
+//Boolen 
+let man = true;
+console.log("He is a man" + man);
 
+//undefine
+let x;
 
+//Null
+let y = null;
+
+//Big int
+let num = BigInt(100000000);
+
+//Symbols
+let unique = Symbol("Saroj!!");
