4ee73794376dfee32c37de97fd778e0c566cbaae
Saroj lama <tamangsarooz100@gmail.com>
2026-06-10 20:48:04 +0545

    Task of Number done

--- CW1.js
+++ CW1.js
@@ -21,15 +21,29 @@ rl.question("Enter your birth year: ", (birthYear) => {
     console.log(`Your age is ${age}`);
 
     rl.close();
-});
+// });
+
 // 3. Calculate Total Price
 // Store the price of a book and the quantity purchased. Calculate and print the total cost.
-
+let bikeprice = 10000 ; 
+let quantity = 10
+let item = bikeprice * quantity;
+console.log(`Total Price of item is ${item}`)
 
 
 // 4. Find the Average
 // Store three test scores in variables and calculate their average.
+ let h1 = 10;
+ let h2 = 30;
+ let h3 = 10;
+ let h4 = 30;
+
+ let sum = h1 + h2 + h3 + h4;
+
+ console.log(`The Average of the total is ${sum/4}`);
 
 
 // 5. Convert Minutes to Seconds
 // Store a number of minutes in a variable and convert it to seconds.
+let time = 5;
+console.log(`let convert minutes into second ${time*60}`);
